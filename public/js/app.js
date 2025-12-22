9c356a20d2fa53cfd31eea92a5096c24a59e1252
664Parvez <parvezrabbi664@gmail.com>
2025-12-22 17:03:10 +0600

    update

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+const visit_btn = document.querySelectorAll(".visit_btn")
+const hover_link = document.querySelectorAll('.hover_link')
+
+visit_btn.forEach((items, visitIndex) => {
+    items.addEventListener("mouseover", (e) => {
+        hover_link.forEach((items, linkIndex) => {
+            if (visitIndex === linkIndex) {
+                
+            } 
+        })
+        e.preventDefault()
+    })
+    items.addEventListener("mouseleave", () => {
+        hover_link.forEach((items, linkIndex) => {
+            if (visitIndex === linkIndex) {
+                
+            }
+        })
+    })
+})
